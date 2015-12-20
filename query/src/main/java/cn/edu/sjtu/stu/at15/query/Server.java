19a7b9dbecd8c02645f40a87aaecbfb40318eb0e
at15 <at15@outlook.com>
2015-12-20 22:27:06 +0800

    Add the query server

---
+++ query/src/main/java/cn/edu/sjtu/stu/at15/query/Server.java
@@ -0,0 +1,15 @@
+package cn.edu.sjtu.stu.at15.query;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by at15 on 15-12-20.
+ */
+public class Server {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
+
+    public void main(String[] args) throws Exception {
+
+    }
+}
